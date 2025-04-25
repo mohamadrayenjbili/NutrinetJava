28d83b5af814b089e66155b449224b5f80736908
mohamadrayenjbili <mohamadrayen.jbili@esprit.tn>
2025-04-25 19:05:10 +0100

    ajout du code promo

--- src/main/java/controllers/Home.java
+++ src/main/java/controllers/Home.java
@@ -20,7 +20,7 @@ public class Home extends Application {
     public void start(Stage stage) {
         try {
             // Charge le formulaire de consultation
-            Parent root = FXMLLoader.load(getClass().getResource("/Produit/AfficherProduitFront.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/CodePromo.fxml"));
 
             Scene scene = new Scene(root);
             stage.setTitle("Ajouter Consultation");
