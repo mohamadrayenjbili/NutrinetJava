36b84694864b3f31a653ba87ac06e5553928b94b
mohamadrayenjbili <mohamadrayen.jbili@esprit.tn>
2025-04-27 18:14:06 +0100

    fixer detail

--- src/main/java/controllers/Produit/DetailProduitController.java
+++ src/main/java/controllers/Produit/DetailProduitController.java
@@ -1,16 +1,21 @@
 package controllers.Produit;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import models.Produit;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class DetailProduitController implements Initializable {
 
@@ -60,4 +65,17 @@ public class DetailProduitController implements Initializable {
             }
         }
     }
+    @FXML
+    private void handleRetour() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Produit/AfficherProduitFront.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Nos Produits");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
