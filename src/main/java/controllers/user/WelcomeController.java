3e4d4620566602d681daa75c7da91e0669ba43a0
SkanderKaraa <skander.karaa@esprit.tn>
2025-04-29 02:22:32 +0100

    Skander : Ajout Tableau Admin + Ajout recherche dynamique recla + Ajout stat AI + ajout API mail lors reponse + ajout metier notation reclamation

--- src/main/java/controllers/user/WelcomeController.java
+++ src/main/java/controllers/user/WelcomeController.java
@@ -1,5 +1,6 @@
 package controllers.user;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -91,6 +92,34 @@ public class WelcomeController {
         }
     }
 
+    @FXML
+    public void naviguateToReclamation(javafx.event.ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/reclamation/listReclamation.fxml"));
+            Parent root = loader.load();
 
+            Stage stage = (Stage) welcomeLabel.getScene().getWindow();
+            stage.setTitle("Reclamation");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    public void naviguateToForum(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/forum/listForum.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) welcomeLabel.getScene().getWindow();
+            stage.setTitle("Forum");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
