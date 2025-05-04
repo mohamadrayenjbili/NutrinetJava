5c0319e0b5bf95c06c88453dbdaaf33e7e11e7b0
mohamadrayenjbili <mohamadrayen.jbili@esprit.tn>
2025-05-04 16:58:56 +0100

    amelioration interface liste_souhait

--- src/main/java/controllers/Home.java
+++ src/main/java/controllers/Home.java
@@ -19,7 +19,7 @@ public class Home extends Application {
         try {
 
 
-            Parent root = FXMLLoader.load(getClass().getResource("/Produit/AfficherProduitFront.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/User/sign_in.fxml"));
 
 
             Scene scene = new Scene(root);
